a9561808029453f4ac6fc002988a962ef8f88711
Daniil Kuliev <danilkakuliew@gmail.com>
2023-10-03 10:42:14 +0300

    add: react-skeleton when loading rooms

--- frontend/src/components/RoomsPage/RoomsPage.tsx
+++ frontend/src/components/RoomsPage/RoomsPage.tsx
@@ -1,10 +1,4 @@
-import {
-  ReactElement,
-  useContext,
-  useEffect,
-  useState,
-  MouseEventHandler,
-} from "react";
+import { ReactElement, useContext, useEffect, useState } from "react";
 import styles from "./RoomsPage.module.css";
 import RoomList from "./RoomList/RoomList";
 import WebSocketContext from "../../context/WebSocketContext";
@@ -13,15 +7,20 @@ import AddRoomBox from "./AddRoomBox/AddRoomBox";
 import CreateRoomModal from "../CreateRoomModal/CreateRoomModal";
 import ApiResponseType from "../../types/ResponseType";
 import Cookies from "js-cookie";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const RoomsPage = (): ReactElement => {
-  const context = useContext(WebSocketContext.wsContext);
   const [rooms, setRooms] = useState<IRoom[]>([]);
+  const [loadingRooms, setLoadingRooms] = useState<boolean>(true);
   const [openCreateRoomModal, setOpenCreateRoomModal] =
     useState<boolean>(false);
 
+  const context = useContext(WebSocketContext.wsContext);
+
   context.on("GetRooms_Receive", (data) => {
     setRooms(JSON.parse(data));
+    setLoadingRooms(false);
   });
 
   context.on("CreateRoom_Receive", (status: ApiResponseType) => {
@@ -44,13 +43,26 @@ const RoomsPage = (): ReactElement => {
     <div className={styles.rooms_layout}>
       <div>
         <h1 className={styles.rooms_layout_title}>Все комнаты</h1>
-        <RoomList rooms={rooms} />
-        <AddRoomBox addRoomHandler={() => setOpenCreateRoomModal(true)} />
-        {openCreateRoomModal && (
-          <CreateRoomModal
-            setModalState={setOpenCreateRoomModal}
-            createRoomHandler={createRoomHandler}
-          />
+        {!loadingRooms ? (
+          <div>
+            <RoomList rooms={rooms} />
+            <AddRoomBox addRoomHandler={() => setOpenCreateRoomModal(true)} />
+            {openCreateRoomModal && (
+              <CreateRoomModal
+                setModalState={setOpenCreateRoomModal}
+                createRoomHandler={createRoomHandler}
+              />
+            )}
+          </div>
+        ) : (
+          <SkeletonTheme baseColor="#11192f" highlightColor="#151e34">
+            <p className={styles.rooms_layout_skeleton}>
+              <Skeleton width={225} height={128} />
+            </p>
+            <p className={styles.rooms_layout_skeleton}>
+              <Skeleton width={225} height={128} />
+            </p>
+          </SkeletonTheme>
         )}
       </div>
     </div>
